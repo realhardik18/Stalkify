53ed340771f7be8190587bf475001346b785131c
realhardik18 <realhardik18@gmail.com>
2024-03-01 23:19:25 +0530

    updated it

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-# MAIN DISCORD BOT FILE
+import subprocess
 from typing import Optional
 import discord
 from discord.ext import commands
 from discord import Member
 from discord.ext.tasks import loop
-from googlesearch import search
-from selenium import webdriver
 
-import time
 
-driver = webdriver.Edge(
-    executable_path='path to executable')
 intents = discord.Intents().all()
 intents.presences = True
 client = commands.Bot(command_prefix="st ", intents=intents)
 client.remove_command('help')
-u = ''
-ind = 0
-curr = ''
-lin = ''
 
+account=None
+prev_song=None
+
+def player(id):
+    subprocess.run(["powershell", "taskkill /im spotify.exe"], capture_output=True)
+    subprocess.run(["powershell", f"start spotify:track:{id}"], capture_output=True)
 
 @client.event
 async def on_ready():
@@ -30,50 +27,25 @@ async def on_ready():
 
 @client.command()
 async def join(ctx, user: Optional[Member]):
-    if ctx.author.id == 'my discord id':
         try:
-            global u, ind, curr, lin
-            u = user
-            for i in range(len(u.activities)):
-                if u.activities[i].name == "Spotify":
-                    ind = i
-                    curr = u.activities[ind].title
-                    query = f"{u.activities[ind].title} {u.activities[ind].artist} youtube lyric video"
-                    for j in search(query, tld="co.in", num=1, stop=1, pause=2):
-                        link = j
-                    indi = link.index('.com')+4
-                    lin = link[0:indi]+"."+link[indi:]
-                    driver.get(lin)
-                    with open('info.txt', 'w') as f:
-                        f.write(
-                            f'{u.display_name}!@!{u.activities[ind].title}!@!{u.activities[ind].artist}!@!{u.activities[ind].album_cover_url}')
-            await ctx.send(f"joined session of {u.display_name}!!")
+            global account
+            account = user
+            await ctx.send(f"i will now stalk {account.display_name} :smiling_imp: :smiling_imp: ")               
         except Exception as e:
             pass
-    else:
-        await ctx.send("i only work for realhardik18 cause he is the best")
 
 
 @loop(seconds=1)
 async def print_info():
-    global curr, u, ind, lin
+    global account,prev_song
     try:
-        if curr != u.activities[ind].title:
-            print(u.activities[ind].title)
-            curr = u.activities[ind].title
-            query = f"{u.activities[ind].title} {u.activities[ind].artist} youtube lyric video"
-            for j in search(query, tld="co.in", num=1, stop=1, pause=2):
-                link = j
-            indi = link.index('.com')+4
-            lin = link[0:indi]+"."+link[indi:]
-            driver.get(lin)
-            with open('info.txt', 'w') as f:
-                f.write(
-                    f'{u.display_name}!@!{u.activities[ind].title}!@!{u.activities[ind].artist}!@!{u.activities[ind].album_cover_url}')
+        if account.activities[0].track_id!=prev_song:
+            player(account.activities[0].track_id)
+            print(F"Now playing {account.activities[0].title}")            
+            prev_song=account.activities[0].track_id
         else:
-            print('same', curr)
+             pass
     except Exception as e:
-        print(e)
         pass
 
-client.run('discord bot token')
+client.run(BOT_TOKEN_HERE)
